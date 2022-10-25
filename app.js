a16e86592762df57e820900884b91fe5378037d6
akulon23 <akulon@lenovo.home>
2022-10-25 15:18:43 +0200

    Data fetching

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 console.log("test");
 const API_URL_ALL = "https://restcountries.com/v3.1/all";
 
-fetch(API_URL_ALL).then(res => res.json())
-.then((res) => console.log(res));
+let countries;
+
+fetch(API_URL_ALL)
+    .then((res) => res.json())
+    .then((countriesRaw) => {
+        countries = countriesRaw.map((country) => {
+            return {
+                capital: country.capital,
+                population: country.population,
+                countryName: country.name.common,
+                region: country.region,
+                flagUrl: country.flags.png,
+            };
+            console.log(countries);
+        });
+    }
+    );
